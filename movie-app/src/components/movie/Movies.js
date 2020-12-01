a4d31d19178dcaaf25faf06b7ecbfcdda702da03
IbraheemLanre <s2001108@edu.bc.fi>
2020-12-01 23:40:56 +0200

    added movie-app functionality and API call

--- movie-app/src/components/movie/Movies.js
+++ movie-app/src/components/movie/Movies.js
@@ -1,13 +1,30 @@
-import { React } from "react";
+import { React, useState } from "react";
 import "./Movies.css";
 
-const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=";
-const API_KEY = process.env.ACCESS_KEY;
-
 const Movies = () => {
+  const [query, setQuery] = useState("");
+  const [movies, setMovies] = useState("");
+
+  const searchMovies = async (e) => {
+    const API_KEY = process.env.REACT_APP_ACCESS_KEY;
+    // const API_URL = `//api.themoviedb.org/3/movie/550?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
+    const API_URL = `http://www.omdbapi.com/?t=${query}&apikey=${API_KEY}`;
+
+    e.preventDefault();
+    console.log("submitting");
+    try {
+      const res = await fetch(API_URL);
+      const data = await res.json();
+      console.log(data);
+      setMovies(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div>
-      <form className="form">
+      <form className="form" onSubmit={searchMovies}>
         <label htmlFor="query" className="label">
           Movie Name
         </label>
@@ -16,11 +33,35 @@ const Movies = () => {
           name="query"
           className="input"
           placeholder="Jurassic Park"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
         />
         <button type="submit" className="button">
-          Submit
+          Search
         </button>
       </form>
+      <div className="card-list">
+        {
+          <div className="card" key={movies.id}>
+            <img
+              className="card--image"
+              src={`${movies.Poster}`}
+              alt={`${movies.Title} poster `}
+            />
+            <div className="card-content">
+              <h3 className="card--title">{movies.Title}</h3>
+              <p>GENRE: {movies.Genre}</p>
+              <p>
+                <small>RELEASE DATE: {movies.Released}</small>
+              </p>
+              <p>
+                <small>RATING: {movies.imdbRating}</small>
+              </p>
+              <p className="card-desc">{movies.Plot}</p>
+            </div>
+          </div>
+        }
+      </div>
     </div>
   );
 };
