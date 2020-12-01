f35d1c9f55db59105afa043b89aa6395cfe215c0
IbraheemLanre <s2001108@edu.bc.fi>
2020-12-02 00:06:22 +0200

    modified movie component

--- movie-app/src/components/movie/Movies.js
+++ movie-app/src/components/movie/Movies.js
@@ -4,65 +4,73 @@ import "./Movies.css";
 const Movies = () => {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const searchMovies = async (e) => {
     const API_KEY = process.env.REACT_APP_ACCESS_KEY;
     const API_URL = `//www.omdbapi.com/?t=${query}&apikey=${API_KEY}`;
 
     e.preventDefault();
-    console.log("submitting");
+    setIsLoading(true);
     try {
       const res = await fetch(API_URL);
       const data = await res.json();
-      console.log(data);
       setMovies(data);
+      setIsLoading(false);
     } catch (err) {
       console.error(err);
     }
   };
 
-  return (
-    <div>
-      <form className="form" onSubmit={searchMovies}>
-        <label htmlFor="query" className="label">
-          Movie Name
-        </label>
-        <input
-          type="text"
-          name="query"
-          className="input"
-          placeholder="Jurassic Park"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <button type="submit" className="button">
-          Search
-        </button>
-      </form>
-      <div className="card-list">
-        {
-          <div className="card" key={movies.id}>
-            <img
-              className="card--image"
-              src={`${movies.Poster}`}
-              alt={`${movies.Title} poster `}
-            />
-            <div className="card-content">
-              <h3 className="card--title">{movies.Title}</h3>
-              <p>GENRE: {movies.Genre}</p>
-              <p>
-                <small>RELEASE DATE: {movies.Released}</small>
-              </p>
-              <p>
-                <small>RATING: {movies.imdbRating}</small>
-              </p>
-              <p className="card-desc">{movies.Plot}</p>
+  let movieList = undefined;
+
+  if (isLoading) {
+    movieList = <p>Loading...</p>;
+  } else {
+    movieList = (
+      <div>
+        <form className="form" onSubmit={searchMovies}>
+          <label htmlFor="query" className="label">
+            Movie Name
+          </label>
+          <input
+            type="text"
+            name="query"
+            className="input"
+            placeholder="Jurassic Park"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button type="submit" className="button">
+            Search
+          </button>
+        </form>
+        <div className="card-list">
+          {
+            <div className="card" key={movies.id}>
+              <img
+                className="card--image"
+                src={`${movies.Poster}`}
+                alt={`${movies.Title} poster `}
+              />
+              <div className="card-content">
+                <h3 className="card--title">{movies.Title}</h3>
+                <p>GENRE: {movies.Genre}</p>
+                <p>
+                  <small>RELEASE DATE: {movies.Released}</small>
+                </p>
+                <p>
+                  <small>RATING: {movies.imdbRating}</small>
+                </p>
+                <p className="card-desc">{movies.Plot}</p>
+              </div>
             </div>
-          </div>
-        }
+          }
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+  return movieList;
 };
 
 export default Movies;
